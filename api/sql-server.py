e3661363de84258042045d30e88f7607c9b256bf
jharia <jharia@trifacta.com>
2016-08-13 21:44:05 -0700

    Updated endpoints, python requirements, .gitignore

--- api/sql-server.py
+++ api/sql-server.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from sqlalchemy import create_engine
 from json import dumps
 
@@ -9,25 +9,49 @@ e = create_engine('sqlite:///./db.sqlite')
 
 app = Flask(__name__)
 api = Api(app)
+parser = reqparse.RequestParser(bundle_errors=True)
 
-class Whd(Resource):
+class CaseList(Resource):
     def get(self):
         conn = e.connect()
-        query = conn.execute("select * from whd_whisard limit 10")
+        stmt = "select * from whd_whisard limit 10"
+        query = conn.execute(stmt)
         result = query.cursor.fetchall()
         keys = result[0]
         rows = result[1:]
 
-        # keys.times(len(rows))
-        # return [zip(keys.repeat(len(rows)), rows)]
-        # return [ {key : v} for row in rows for key,v in keys, row ]
-        # return { keys[i]: [rows[j][i] for j in range(len(rows))] for i in range(len(rows))}
-        return [ {k : v} for i in range(len(rows)) for k,v in zip(keys, rows[i])  ]
+        return_result = []
 
+        for row in rows:
+            result_dict = {}
+            for key, value in zip(keys, row):
+                result_dict[key] = value
+            return_result.append(result_dict)
 
-        # return {'cases': [i for i in query.cursor.fetchall()]}
+        return return_result
 
-api.add_resource(Whd, '/cases')
+class Case(Resource):
+    def get(self, case_id=None):
+        conn = e.connect()
+        stmt = "select * from whd_whisard"
+        if (case_id is not None):
+            stmt += " where case_id=" + str(case_id)
+        query = conn.execute(stmt)
+
+        result = query.cursor.fetchall()[0]
+        key = conn.execute('select * from whd_whisard limit 1').cursor.fetchall()[0]
+        result_dict = {}
+
+        for key, value in zip(key, result):
+            result_dict[key] = value
+        return result_dict
+
+# Add all REST definitions here
+api.add_resource(Case, '/cases/<case_id>')
+api.add_resource(CaseList, '/cases')
+# api.add_resource(CasesByCounty, '/counties/<county_id>/cases')
+# api.add_resource(CasesByNAICId3, '/naic_id_3/<naic_id_2>/cases')
+
 
 if __name__ == '__main__':
     app.run()
