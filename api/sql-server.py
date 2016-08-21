b64e7f2f01ee8305ad5f6008171d5c4e329d851c
michael-eastwood <eastwoodlark@gmail.com>
2016-08-20 22:58:45 -0700

    Added counties resource
    This returns a CSV file with back wage information for each county in
    the US in the format currently used by our D3 JavaScript visualization.
    A better approach would probably be to change the visualization to read
    JSON instead. Also, the file really should be cashed to disk so
    probably it makes more sense to take it out of the API and instead have
    it be created by the script we create to update the sqlite database
    with new data. That script can also recreate the static CSV or son file
    at the same time, using this query, and then we can take this out of
    the API.

--- api/sql-server.py
+++ api/sql-server.py
@@ -2,7 +2,7 @@
 
 from json import dumps
 
-from flask import Flask, request
+from flask import Flask, request, Response
 from flask_restful import Resource, Api, reqparse
 from sqlalchemy import create_engine
 
@@ -13,6 +13,7 @@ e = create_engine('sqlite:///./db.sqlite')
 app = Flask(__name__)
 api = Api(app)
 parser = reqparse.RequestParser(bundle_errors=True)
+
 
 class CaseList(Resource):
     def get(self):
@@ -91,12 +92,47 @@ class CasesByNAICCd(Resource):
 
         return return_result
 
+class CountySummaries(Resource):
+    def get(self, naic_cd=None):
+        conn = e.connect()
+        stmt = """SELECT [Combined State County FIPS], 
+        Sum(Backwages) as Backwages, Sum([Employees Owed Backwages]) as [Employees Owed Backwages],
+        Sum([Civil Money Penalties]) as [Civil Money Penalties], County
+        FROM cases
+        WHERE [Combined State County FIPS] IS NOT NULL
+        GROUP BY [Combined State County FIPS], County
+        ORDER BY Backwages Desc;"""
+        
+
+        query = conn.execute(stmt)
+        rows = query.cursor.fetchall()
+        keys = [member[0] for member in query.cursor.description]
+        print keys, rows
+
+        return_result = []
+        
+        import cStringIO, csv
+        dest = cStringIO.StringIO()
+        writer = csv.writer(dest)
+
+        #WRITE THE CSV FILE IN THE FORMAT WE NEED. CURRENTLY WE DON'T HAVE total_pop SO
+        #FOR NOW I'M SUBSTITUTING CMPs AS A PLACEHOLDER.
+        writer.writerow(['id', 'wages', 'count', 'total_pop', 'name'])
+        
+        writer.writerows(rows)
+
+            
+        return Response(dest.getvalue(), mimetype='text/csv')           
+        dest.close()
+            
+#       return return_result
+
 # Add all REST definitions here
 api.add_resource(Case, '/cases/<case_id>')
 api.add_resource(CaseList, '/cases')
 api.add_resource(CasesByZip, '/zip_cd/<zip_cd>/cases')
 api.add_resource(CasesByNAICCd, '/naic_cd/<naic_cd>/cases')
-
+api.add_resource(CountySummaries, '/counties')
 
 if __name__ == '__main__':
     app.run()
